2ecb36f8d417b1acf3b378413ab59ba8d8b7c998
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-04-07 20:23:24 +0000

    changed beginGame

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // add javascript here
 let userName = prompt("Please enter your name.");
 userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase(); //capitalizes the first letter and makes the rest lowercase
-let difficultyLevel = document.getElementsByName("level");
 let play = document.getElementById("playBtn");
-let range = difficultyLevel.value();
 play.addEventListener("click", beginGame);
 function beginGame(){
+  let difficultyLevel = document.getElementsByName("level");
+  let range = 3;
+  for (let i = 0; i<difficultyLevel.length; i++){
+    if (difficultyLevel[i].checked){
+        range = parseint(difficultyLevel[i].value);
+    }
+  }
+  console.log("Range:" + range);
   let randNum = Math.floor(Math.random() * range) + 1;  
 }

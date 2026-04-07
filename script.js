00de3d64bbc6bf7ae2ac5e887766a1a139e8dd4a
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-04-07 21:12:32 +0000

    completed play button code and started on guessing w/ feedback code

--- script.js
+++ script.js
@@ -2,15 +2,43 @@
 let userName = prompt("Please enter your name.");
 userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase(); //capitalizes the first letter and makes the rest lowercase
 let play = document.getElementById("playBtn");
+
+let randNum;
+let guess = document.getElementById("guess");
+let message = document.getElementById("msg");
+
 play.addEventListener("click", beginGame);
 function beginGame(){
   let difficultyLevel = document.getElementsByName("level");
   let range = 3;
   for (let i = 0; i<difficultyLevel.length; i++){
     if (difficultyLevel[i].checked){
-        range = parseint(difficultyLevel[i].value);
+        range = parseInt(difficultyLevel[i].value);
     }
   }
-  console.log("Range:" + range);
-  let randNum = Math.floor(Math.random() * range) + 1;  
+  randNum = Math.floor(Math.random() * range) + 1;  
+  let guess = document.getElementById("guess");
+  let guessButton = document.getElementById("guessBtn");
+  let giveUpButton = document.getElementById("giveUpBtn");
+  let message = document.getElementById("msg");
+  guessButton.disabled = false;
+  giveUpButton.disabled = false;
+  play.disabled = true;
+  message.innerText = ("Type your first guess!");
+  alert("Range:" + range);
+  alert("RandNum:" + randNum);
+  guessButton.addEventListener("click", checkGuess);
+}
+
+
+function checkGuess(){
+  if (Number(guess.value)>randNum){
+    message.innerText = ("Too high!");
+  }else if (Number(guess.value)<randNum){
+    message.innerText = ("Too low!");
+  }else if (Number(guess.value) == randNum){
+    message.innerText = ("Correct!");
+  }else{
+    message.innerText = ("Please type a Number!");
+  }
 }

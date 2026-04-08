7dcdf32db776a5a644cc9738d2abe10d82115a9c
pranav-vora <149259449+pranav-vora@users.noreply.github.com>
2026-04-08 21:13:30 +0000

    added giveup and finished leaderboard, fixed function names

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if (userName === null) {
   userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase(); //capitalizes the first letter and makes the rest lowercase
 }
 
-let play = document.getElementById("playBtn");
+let playBtn = document.getElementById("playBtn");
 
 let randNum;
 let message = document.getElementById("msg");
@@ -15,65 +15,98 @@ let winsText = document.getElementById("wins");
 let wins = 0;
 let numberOfGuesses = 0;
 let guessesPerWin = [];
-play.addEventListener("click", beginGame);
-function beginGame(){
+let guessButton = document.getElementById("guessBtn");
+let giveUpButton = document.getElementById("giveUpBtn");
+let range = 3;
+playBtn.addEventListener("click", play);
+function play(){
   let difficultyLevel = document.getElementsByName("level");
-  let range = 3;
   for (let i = 0; i<difficultyLevel.length; i++){
     if (difficultyLevel[i].checked){
         range = parseInt(difficultyLevel[i].value);
     }
   }
   randNum = Math.floor(Math.random() * range) + 1;  
-  let guessButton = document.getElementById("guessBtn");
-  let giveUpButton = document.getElementById("giveUpBtn");
+
   guessButton.disabled = false;
   giveUpButton.disabled = false;
-  play.disabled = true;
+  playBtn.disabled = true;
   message.innerText = (userName + ", type your first guess!");
   // alert("Range:" + range);
   // alert("RandNum:" + randNum);
-  guessButton.addEventListener("click", checkGuess);
+  guessButton.addEventListener("click", makeGuess);
+  document.getElementById("giveUpBtn").addEventListener("click", giveUp);
 }
 
 
-function checkGuess(){
+function makeGuess(){
   let guess = document.getElementById("guess");
   let proximity = Math.abs(Number(guess.value)-randNum);
   numberOfGuesses++;
   if (Number(guess.value)>randNum){
 
     if(proximity<=2){
-      message.innerText = ("Too high, but hot!");
+      message.innerText = ("Too high, but you are hot!");
     }else if (proximity<=5){
-      message.innerText = ("Too high, but warm!");
+      message.innerText = ("Too high, but you are warm!");
     }else{
-      message.innerText = ("Too high, and cold!");
+      message.innerText = ("Too high, and you are cold!");
     }
 
   }else if (Number(guess.value)<randNum){
 
     if(proximity<=2){
-      message.innerText = ("Too low, but hot!");
+      message.innerText = ("Too low, but you are hot!");
     }else if (proximity<=5){
-      message.innerText = ("Too low, but warm!");
+      message.innerText = ("Too low, but you are warm!");
     }else{
-      message.innerText = ("Too low, and cold!");
+      message.innerText = ("Too low, and you are cold!");
     }
 
   }else if (Number(guess.value) == randNum){
     message.innerText = ("Correct! Good Job, " + userName + "!");
-    guessesPerWin.push(numberOfGuesses);
+    updateScore(numberOfGuesses);
 
-    let sum = 0;
-    for (let num of guessesPerWin) {
-        sum += num;
-    }
-    const average = sum / guessesPerWin.length;
-    wins ++;
-    winsText.innerText = ("Total wins: "+ wins);
-    document.getElementById("avgScore").innerText = ("Average Score: " + average);
   }else{
     message.innerText = ("Please type a Number!");
   }
+}
+
+
+function giveUp(){
+  updateScore(range);
+}
+
+
+function updateScore(score){
+  guessesPerWin.push(score); //adds the current number of guesses to the array
+  guessesPerWin.sort((a, b) => a - b); //sorts numbers in ascending order
+  numberOfGuesses = 0; //resets it to 0 if you win
+
+  reset() //enables play button and disables guess and giveup buttons
+
+  let sum = 0;
+  for (let num of guessesPerWin) {
+      sum += num;
+  }
+  const average = sum / guessesPerWin.length;
+  wins ++;
+  winsText.innerText = ("Total wins: "+ wins);
+  document.getElementById("avgScore").innerText = ("Average Score: " + average);
+  let leaderboardElements=document.getElementsByName("leaderboard");
+
+  for(let i = 0; i<3; i++){
+    leaderboardElements[i].textContent= guessesPerWin[i];
+  }
+}
+
+function displayDate(){
+  let dateText = document.getElementById("date");
+  
+}
+
+function reset(){
+  guessButton.disabled = true;
+  giveUpButton.disabled = true;
+  playBtn.disabled = false;
 }
